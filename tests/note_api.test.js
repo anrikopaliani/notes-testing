594d0db1ceddc42b550d8f6433f9807c59108d7a
kopala123 <anri.kopaliani.1@btu.edu.ge>
2023-12-28 22:59:44 +0400

    feat:initialize database before tests

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,8 +1,27 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
-
+const Note = require("../models/note");
 const api = supertest(app);
+
+const initialNotes = [
+  {
+    content: "HTML is easy",
+    important: false,
+  },
+  {
+    content: "Browser can execute only JavaScript",
+    important: true,
+  },
+];
+
+beforeEach(async () => {
+  await Note.deleteMany({});
+  let noteObject = new Note(initialNotes[0]);
+  await noteObject.save();
+  noteObject = new Note(initialNotes[1]);
+  await noteObject.save();
+});
 
 test("notes are returned as json", async () => {
   await api
